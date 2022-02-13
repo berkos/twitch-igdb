a233a092974db8ffdaf02195fed71605dcadc479
Antonis Berkakis <aberkakis@gmail.com>
2022-02-13 20:48:37 +0000

    Create gem template

---
+++ lib/twitch/igdb/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Twitch
+  module Igdb
+    VERSION = "0.0.0"
+  end
+end
---
+++ spec/twitch/igdb_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Twitch::Igdb do
+  it "has a version number" do
+    expect(Twitch::Igdb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
